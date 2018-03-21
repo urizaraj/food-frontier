0c831887877a31afd02b97470cef2c2812934b3f
urizaraj <32577918+urizaraj@users.noreply.github.com>
2018-03-21 14:34:13 -0400

    Add item to item tags join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321171624) do
+ActiveRecord::Schema.define(version: 20180321174848) do
 
   create_table "item_tags", force: :cascade do |t|
     t.string "name"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "item_to_item_tags", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "item_tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
