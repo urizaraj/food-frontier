9d03979161f7e243200ca3ad13771e52d51ba8a4
urizaraj <32577918+urizaraj@users.noreply.github.com>
2018-03-21 14:34:29 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = [['Burger Planet',
+                'The best burgers on the planet'],
+               ['Pizza That!',
+                'Pizza with all the toppings you can think of'],
+               ['Ooh, Organic',
+                'Fresh and delicious organic food'],
+               ['Breakfast & Breakloose',
+                'Great breakfast foods'],
+               ['SubSolutions',
+                'Subs and sandwiches']]
+
+restaurants.each do |name, description|
+  Restaurant.create(name: name, description: description)
+end
+
+items = ['Hamburger',
+         'Cheeseburger',
+         'Hotdog',
+         'Fries',
+         'Iced Tea',
+         'Soda',
+         'Pizza',
+         'Salad',
+         'Bean Burger',
+         'Omelette',
+         'Pancakes',
+         'Juice',
+         'Coffee',
+         'Sub Sandwhich',
+         'Club Sandwhich']
+
+items.each do |item|
+  Item.create(name: item)
+end
