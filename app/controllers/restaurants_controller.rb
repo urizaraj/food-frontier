f91a15400b869d933b735add7910cb076bd7c184
urizaraj <32577918+urizaraj@users.noreply.github.com>
2018-03-21 15:07:11 -0400

    Restaurant controller actions

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,35 @@
+class RestaurantsController < ApplicationController
+  before_action :set_resource, only: %i[show edit update]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(strong_params)
+
+    return render :new unless @restaurant.save
+
+    redirect_to @restaurant
+  end
+
+  def update
+    @restaurant.update(strong_params)
+
+    redirect_to @restaurant
+  end
+
+  private
+
+  def strong_params
+    params.require(:restaurant).permit(:name, :description, :category)
+  end
+
+  def set_resource
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
