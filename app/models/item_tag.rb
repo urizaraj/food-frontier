0f6034e7efda6649035c42aa9f25188952bb401a
Alexander Urizar <urizaraj@gmail.com>
2018-03-22 14:19:31 -0400

    Entree, side, and drink grouping

--- app/models/item_tag.rb
+++ app/models/item_tag.rb
@@ -1,4 +1,12 @@
 class ItemTag < ApplicationRecord
   has_many :item_to_item_tags
   has_many :items, through: :item_to_item_tags
+
+  def self.main
+    where(name: %w[Entree Side Drink])
+  end
+
+  def self.other
+    where.not(name: %w[Entree Side Drink])
+  end
 end
