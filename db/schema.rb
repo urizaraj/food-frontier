49ec09c162515776ec14308a69820955bc84c5d7
Alexander Urizar <urizaraj@gmail.com>
2018-03-22 15:58:19 -0400

    Now users have names

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322191825) do
+ActiveRecord::Schema.define(version: 20180322195637) do
 
   create_table "item_tags", force: :cascade do |t|
     t.string "name"
@@ -49,6 +49,16 @@ ActiveRecord::Schema.define(version: 20180322191825) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "rating"
+    t.integer "user_id"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -62,6 +72,7 @@ ActiveRecord::Schema.define(version: 20180322191825) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
