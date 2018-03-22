b24cc9c954510bc94fa002fa0efbc5b61c5095f6
Alexander Urizar <urizaraj@gmail.com>
2018-03-22 15:57:52 -0400

    Update models with reviews

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,7 @@ class Restaurant < ApplicationRecord
   has_many :menu_items
   has_many :items, through: :menu_items
   has_many :item_tags, through: :items
+  has_many :reviews
 
   accepts_nested_attributes_for :menu_items
 end
