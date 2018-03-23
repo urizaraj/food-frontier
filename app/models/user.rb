1d8d6c26db87ad4327081b3bebc0bd353559f2d1
Alexander Urizar <urizaraj@gmail.com>
2018-03-23 12:00:15 -0400

    Allow login by name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,21 @@
 class User < ApplicationRecord
+  attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :reviews
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates_format_of :name, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(['lower(name) = :value OR lower(email) = :value', { value: login.downcase }]).first
+    elsif conditions.key?(:name) || conditions.key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
