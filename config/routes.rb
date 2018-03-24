9ec0a728420f85502782ca5a8705b9363e000fc6
Alexander Urizar <urizaraj@gmail.com>
2018-03-24 14:55:11 -0400

    Limit reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,15 @@ Rails.application.routes.draw do
 
   resources :restaurants do
     resources :menu_items
-    resources :reviews
+    resources :reviews, only: %i[index create edit update destroy]
   end
+
   get 'restaurants/:id/set_prices', to: 'restaurants#set_prices', as: 'prices_restaurant'
 
   resources :items
   resources :item_tags
 
   resources :users, only: [:show] do
-    resources :reviews, only: %i[index show]
+    resources :reviews, only: %i[index edit update destroy]
   end
 end
