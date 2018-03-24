e59e694dbad365660246def2e7449769f30b7125
Alexander Urizar <urizaraj@gmail.com>
2018-03-24 15:54:26 -0400

    Render rating helper method

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -2,4 +2,8 @@ module RestaurantsHelper
   def render_menu_items(restaurant, symbol)
     render partial: 'menu_item', locals: {restaurant: restaurant, sym: symbol}
   end
+
+  def render_rating(restaurant)
+    restaurant.rating.zero? ? tag.small('no reviews') : restaurant.rating.round(2)
+  end
 end
