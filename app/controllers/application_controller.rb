266ee4092d42650989d268413bd58bce721d26b9
Alexander Urizar <urizaraj@gmail.com>
2018-03-24 14:53:19 -0400

    New controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def require_login
+    return head(:forbidden) unless user_signed_in?
+  end
+
+  def require_admin
+    return head(:forbidden) unless admin?
+  end
+
+  def admin?
+    user_signed_in? ? current_user.admin : false
+  end
+
   protected
 
   def configure_permitted_parameters
