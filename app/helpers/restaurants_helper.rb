ef429c094137c95e8987f346d3ccada395673902
Alexander Urizar <urizaraj@gmail.com>
2018-03-27 11:20:37 -0400

    Use a helper to display a menu item price

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -6,4 +6,8 @@ module RestaurantsHelper
   def render_rating(restaurant)
     restaurant.rating.zero? ? tag.small('no reviews') : restaurant.rating.round(2)
   end
+
+  def disp_price(menu_item)
+    "$#{menu_item.price}" if menu_item.price
+  end
 end
