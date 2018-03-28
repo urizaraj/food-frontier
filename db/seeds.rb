a8cc57e73c44b5852799a10873c28f7cbbf0909e
Alexander Urizar <urizaraj@gmail.com>
2018-03-28 13:27:14 -0400

    Better seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,37 +6,54 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-restaurants = [['Burger Planet',
-                'The best burgers on the planet'],
-               ['Pizza That!',
-                'Pizza with all the toppings you can think of'],
-               ['Ooh, Organic',
-                'Fresh and delicious organic food'],
-               ['Breakfast & Breakloose',
-                'Great breakfast foods'],
-               ['SubSolutions',
-                'Subs and sandwiches']]
+restaurants = [
+  ['Burger Planet', 'The best burgers on the planet'],
+  ['Pizza That!', 'Pizza with all the toppings you can think of'],
+  ['Ooh, Organic', 'Fresh and delicious organic food'],
+  ['Breakfast & Breakloose', 'Great breakfast foods'],
+  ['SubSolutions', 'Subs and sandwiches']
+]
 
 restaurants.each do |name, description|
   Restaurant.create(name: name, description: description)
 end
 
-items = ['Hamburger',
-         'Cheeseburger',
-         'Hotdog',
-         'Fries',
-         'Iced Tea',
-         'Soda',
-         'Pizza',
-         'Salad',
-         'Bean Burger',
-         'Omelette',
-         'Pancakes',
-         'Juice',
-         'Coffee',
-         'Sub Sandwhich',
-         'Club Sandwhich']
+entree = ItemTag.create(name: 'Entree')
+side = ItemTag.create(name: 'Side')
+drink = ItemTag.create(name: 'Drink')
 
-items.each do |item|
-  Item.create(name: item)
+entrees = [
+  'Hamburger',
+  'Cheeseburger',
+  'Hotdog',
+  'Pizza',
+  'Bean Burger',
+  'Salad',
+  'Omelette',
+  'Pancakes',
+  'Sub Sandwich',
+  'Club Sandwich'
+]
+
+drinks = [
+  'Soda',
+  'Iced Tea',
+  'Juice',
+  'Coffee',
+]
+
+entrees.each do |name|
+  item = Item.new(name: name)
+  item.item_tags << entree
+  item.save
 end
+
+drinks.each do |name|
+  item = Item.new(name: name)
+  item.item_tags << drink
+  item.save
+end
+
+fries = Item.new(name: 'Fries')
+fries.item_tags << side
+fries.save
